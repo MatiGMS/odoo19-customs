6a432d9acaef25827d6b0fd53cb9388c849a363e
Matias García <magarciam@fi.uba.ar>
2026-06-03 10:46:25 -0500

    Update __manifest__.py

---
+++ campo_en_taller_deco/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "KPI Shop Floor Specs",
+    "version": "19.0.1.0.0",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp_workorder",
+        "mrp",
+    ],
+    "data": [
+        "views/shop_floor_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
