4cb05bf0f2309ca264165b590adfef5875349041
Matias García <magarciam@fi.uba.ar>
2026-06-15 10:38:59 -0500

    Update __manifest__.py

--- campo_delivery_type_deco/__manifest__.py
+++ campo_delivery_type_deco/__manifest__.py
@@ -1,8 +1,14 @@
 {
-    "name": "MRP BoM Taller Info",
-    "depends": ["mrp"],
-    "data": [
-        "views/mrp_production_templates.xml",
+    "name": "MRP Workorder Tags from Production",
+    "summary": "Muestra las etiquetas de la orden de producción en la vista de taller, encima de Notas.",
+    "version": "19.0.1.0.0",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp",
     ],
+    "data": [
+        "campo_delivery_type_deco/views/mrp_production_templates.xml",
+    ],
+    "license": "AGPL-3",
     "installable": True,
-}   
+}
